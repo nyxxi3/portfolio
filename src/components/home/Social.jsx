bd0681df4cc1f936e3a4559fdd345a90f1ba7f33
Jeevitha <jeevi.r2003@gmail.com>
2025-07-12 16:20:44 +0530

    initial commit from scratch

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+           
+            <a href="https://www.linkedin.com/in/jeevitharavi21/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-linkedin-alt"></i>
+            </a>
+
+            <a href="https://github.com/nyxxi3" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,56 @@
+import Work1 from '../../assets/work1.jpg'
+import Work2 from '../../assets/work2.jpg'
+import Work3 from '../../assets/work3.jpg'
+import Work4 from '../../assets/work4.jpg'
+import Work5 from '../../assets/work5.jpg'
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title: "Sleep Apnea Detection",
+        category: "ML",
+        link: "https://github.com/nyxxi3/sleep-apnea-detection"
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: "Selenium Automation",
+        category: "Selenium",
+        link: "https://github.com/nyxxi3/Selenium-Automation-College-Project"
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: "Sathyabama News Bot",
+        category: "Python",
+        link: "https://github.com/nyxxi3/telegram-bot"
+    },
+    // {
+    //     id: 4,
+    //     image: Work4,
+    //     title: "App movil",
+    //     category: "app",
+    // },
+    // {
+    //     id: 5,
+    //     image: Work5,
+    //     title: "Web design",
+    //     category: "Web",
+    // },
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'ML',
+    },
+    {
+        name: 'Selenium',
+    },
+    {
+        name: 'Python',
+    },
+];
